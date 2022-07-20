e6867310eb18f91bb11d9f4aec606672cc2d2144
Reimar Rosas <reimarrosas0208@gmail.com>
2022-07-20 12:50:37 -0400

    Initial files

---
+++ src/main/java/me/reimarrosas/strelloapi/StrelloApiApplication.java
@@ -0,0 +1,13 @@
+package me.reimarrosas.strelloapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrelloApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StrelloApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/reimarrosas/strelloapi/StrelloApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.reimarrosas.strelloapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrelloApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
